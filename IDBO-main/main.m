027d8293e2b520102c152621e7bfcb30b776b887
changzhanyou0215 <112379739+changzhanyou0215@users.noreply.github.com>
2025-06-22 22:13:31 +0800

    main.m

--- IDBO-main/main.m
+++ IDBO-main/main.m
@@ -4,75 +4,28 @@
 % Chang Z, Luo J. EIDBO-TrICP: a coarse-to-fine algorithm for cross-source point cloud registration in aero-engineering blades. Engineering Research Letters. 2025; 7: 015280.
 % DOI: https://doi.org/10.1088/2631-8695/adb4bf
 
-clear all;
-close all;
-clc;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all 
+close all
+clc
 
-SearchAgents_no = 30;    % population size
-Function_name = 'F1';    % Test Functions: F1-F23 
-Max_iteration = 500;     % Maximum Iterations
-cnt_max = 1;             % Runs
+SearchAgents_no=30; % Number of search agents
 
-[lb, ub, dim, fobj] = Get_Functions_details(Function_name); % Load details of selected test function
+Function_name='F1'; % Name of the test function that can be from F1 to F23 (Table 1,2,3 in the paper)
 
+Max_iteration=500; % Maximum numbef of iterations
 
-Curve_DBO = zeros(1, Max_iteration);
-Curve_IDBO = zeros(1, Max_iteration);
-
- 
-for cnt = 1 : cnt_max
-
-
-    [DBO_Best_score(cnt),    DBO_Best_pos(cnt,:),   DBO_curve]   =   DBO(SearchAgents_no, Max_iteration, lb, ub, dim, fobj);      % DBO
-    [IDBO_Best_score(cnt),   IDBO_Best_pos(cnt,:),  IDBO_curve]  =  IDBO(SearchAgents_no, Max_iteration, lb, ub, dim, fobj);      % IDBO
-    
-    
-    Curve_DBO = Curve_DBO + DBO_curve;
-	Curve_IDBO = Curve_IDBO + IDBO_curve;
-
-end
-
-% Iteration curve
-Curve_DBO = Curve_DBO/cnt_max;
-Curve_IDBO = Curve_IDBO/cnt_max;
-
-
-% Std
-std_DBO = std(DBO_Best_score);
-std_IDBO = std(IDBO_Best_score);
-
-
-% Worst
-worst_DBO = max(DBO_Best_score);
-worst_IDBO = max(IDBO_Best_score);
-
-
-% Best
-best_DBO = min(DBO_Best_score);
-best_IDBO = min(IDBO_Best_score);
-
-
-% Mean
-mean_DBO = mean(DBO_Best_score);
-mean_IDBO = mean(IDBO_Best_score);
-
-
-figure(1)
-semilogy(Curve_DBO,'color', [125, 195, 254]./255, 'linestyle', '-', 'linewidth', 2)
-hold on
-semilogy(Curve_IDBO,'color', [118, 80, 5]./255, 'linestyle', '-', 'linewidth', 1.5)
-
-title(Function_name,'FontName', 'Helvetica', 'FontSize', 12);
-xlabel('Iteration', 'FontName', 'Helvetica', 'FontSize', 12);
-ylabel('Best score obtained so far', 'FontName', 'Helvetica', 'FontSize', 12);
-set(gca,'linewidth',2);
+% Load details of the selected benchmark function
+[lb,ub,dim,fobj]=Get_Functions_details(Function_name);
+[fMin,bestX,IDBO_curve]=IDBO(SearchAgents_no,Max_iteration,lb,ub,dim,fobj);
+semilogy(IDBO_curve,'Color','g')
+title('Objective space')
+xlabel('Iteration');
+ylabel('Best score obtained so far');
+%axis tight
 grid on
 box on
-legend('DBO','IDBO')% Comparison of different metaheuristics
-grid off
-
-
-% Result Outputs
-disp(['DBO£ºWorst: ', num2str(worst_DBO), ', Best: ', num2str(best_DBO), ', Mean: ', num2str(mean_DBO), ', Std: ', num2str(std_DBO)]);
-disp(['IDBO£ºWorst: ', num2str(worst_IDBO), ', Best: ', num2str(best_IDBO), ', Mean: ', num2str(mean_IDBO), ', Std: ', num2str(std_IDBO)]);
+legend('IDBO')
+display(['The best solution obtained by IDBO is : ', num2str(bestX)]);
+display(['The best optimal value of the objective funciton found by IDBO is : ', num2str(fMin)]);
 
